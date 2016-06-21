18e1c6286ac376360d614752dffa122c0bf2e7dd
Ahkeem Lang <langahkeem@gmail.com>
2016-06-20 21:01:00 -0500

    Made 11 assertions

--- bob.rb
+++ bob.rb
@@ -1,2 +1,13 @@
 class Bob
+  def hey(remark)
+    if remark.upcase == remark
+      "Whoa, chill out!"
+    elsif  remark =~ /[\?]$/  # Find out if remark is asking a question
+      "Sure."
+    elsif remark.empty?
+      "Sure"
+    else
+      "Whatever."
+    end
+  end
 end
